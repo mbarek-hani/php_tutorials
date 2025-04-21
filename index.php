655ad0b326c1fcb03ab7397c2d0cdec58be35d9d
mbarek-hani <mbarekhani30@gmail.com>
2025-04-21 22:11:42 +0100

    assignement by reference

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
+<?php
+    $x = "Hello, world!";
+    $y = &$x; // assignement by reference
+    $x = 15; // $y also changes
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
         echo "<pre>";
-        print_r($_SERVER);
+        var_dump($y); // outputs 15
         echo "</pre>";
     ?>
-
-    <script src="index.js"></script>
 </body>
-</html>
 
+</html>
